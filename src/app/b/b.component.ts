f643d215729fa73de95130a317e215fa557f9756
Omer <omer.mohamed@sheridancollege.ca>
2025-04-03 12:43:05 -0400

    get/post data from/to Spring App API

--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../../Student';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-b',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './b.component.html',
   styleUrl: './b.component.css'
 })
 export class BComponent {
 // get, update delete students info 
+
+students : Student[]= []
+service :DataService
+constructor(service : DataService ){
+  this.service = service
+}
+
+st : Student = {id : 0 , name :"Reem" , age : 22}
+getData(){
+
+ let ob = this.service.getStudents()
+
+ ob.subscribe(res =>{
+  console.log()
+  this.students = <Student[]>res;
+ })
+}
+
+addStudent(){
+
+  this.service.addStudent(this.st)
+}
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Student } from '../Student';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +20,24 @@ export class DataService {
     console.log(res)
    })
     return ob
+   }
+
+
+   getStudents(){
+
+    let ob = this.http.get("http://localhost:8080/api/students")
+
+    return ob
+   }
+
+   addStudent( s : Student){
+    
+   let url = "http://localhost:8080/api/students"
+let ob =this.http.post(url , s)
+  ob.subscribe(res => {
+
+    console.log(res)
+    alert("done")
+  })
    }
 }
